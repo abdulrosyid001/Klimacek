d0622239c09ba7a95ba9ba16aa5bd845f657a50d
Abdul Rosyid <abdulrosyid6122004@gmail.com>
2025-08-26 03:48:38 +0000

    Edit Logo Partner

--- frontend/data/partners.ts
+++ frontend/data/partners.ts
@@ -1,5 +1,8 @@
 export const partners = [
-  {logo: '/images/partner-1.png' },
-  {logo: '/images/partner-2.png' },
-  {logo: '/images/partner-3.png' }
+  { name: 'AgriTech Hub', logo: '/images/partner-1.png' },
+  { name: 'GreenFarm', logo: '/images/partner-2.png' },
+  { name: 'IoT Incubator', logo: '/images/partner-3.png' },
+  { name: 'ClimateX', logo: '/images/partner-4.png' },
+  { name: 'Farm Innovators', logo: '/images/partner-5.png' },
+  { name: 'AgroFuture', logo: '/images/partner-6.png' },
 ];
