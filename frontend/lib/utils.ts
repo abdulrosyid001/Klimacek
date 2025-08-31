5527404b884c40568367d51c5e92be6ef44e3d3f
Abdul Rosyid <abdulrosyid6122004@gmail.com>
2025-08-31 15:05:05 +0000

    Edit dashboard

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,5 +1,5 @@
-import { clsx } from "clsx"
-import { type ClassValue, twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
